740145c0366e4cc190e987d9b9ffc14ef5fd5132
Sam Boysel <sboysel@gmail.com>
2023-01-08 22:55:56 -0800

    [lily58] keymap tweaks

--- keyboards/lily58/keymaps/sboysel/config.h
+++ keyboards/lily58/keymaps/sboysel/config.h
@@ -29,22 +29,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
-#undef RGBLED_NUM
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
-#define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#define RGBLIGHT_EFFECT_RGB_TEST
-#define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+/* #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader */
+
+/* #undef RGBLED_NUM */
+/* #define RGBLIGHT_EFFECT_BREATHING */
+/* #define RGBLIGHT_EFFECT_RAINBOW_MOOD */
+/* #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
+/* #define RGBLIGHT_EFFECT_SNAKE */
+/* #define RGBLIGHT_EFFECT_KNIGHT */
+/* #define RGBLIGHT_EFFECT_CHRISTMAS */
+/* #define RGBLIGHT_EFFECT_STATIC_GRADIENT */
+/* #define RGBLIGHT_EFFECT_RGB_TEST */
+/* #define RGBLIGHT_EFFECT_ALTERNATING */
+/* #define RGBLIGHT_EFFECT_TWINKLE */
+/* #define RGBLED_NUM 27 */
+/* #define RGBLIGHT_LIMIT_VAL 120 */
+/* #define RGBLIGHT_HUE_STEP 10 */
+/* #define RGBLIGHT_SAT_STEP 17 */
+/* #define RGBLIGHT_VAL_STEP 17 */
 
 // Underglow
 /*
--- keyboards/lily58/keymaps/sboysel/keymap.c
+++ keyboards/lily58/keymaps/sboysel/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_RAISE), KC_BSPC, KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                       KC_LCTL, KC_LGUI, MO(_LOWER), KC_SPC,     KC_ENT,  MO(_RAISE), KC_RGUI, KC_RALT
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,       KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______, KC_BSPC, KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                       KC_LCTL, KC_LGUI, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_RGUI, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -37,19 +37,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  Del |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  -   |  =   |  [   |  ]   |      |-------.    ,-------| Home | PgDn | PgUp | End  |      |      |
+ * |      |  -   |  =   |  [   |  ]   |      |-------.    ,-------| Home | PgDn | PgUp | End  |      |  -   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      | Mute | Vol- | Vol+ |      |      |
+ * |      |      |      |      |      |      |-------|    |-------|      | Mute | Vol- | Vol+ |      |  =   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \BackSP\  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______,  _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,  _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______,    _______,           _______,         _______, _______,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,       KC_8,              KC_9,            KC_0,    KC_MINS,
+  _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,                   KC_HOME, KC_PGDN,    KC_PGUP,           KC_END,          _______, KC_EQL,
+  _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, KC_KB_MUTE, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, _______, _______,
                              _______, _______, _______, _______, KC_BSPC, _______, _______, _______
 ),
 /* RAISE
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, QK_MAKE, KC_BRID, KC_BRIU, _______, _______,
+  KC_CAPS, _______, _______, _______, _______, _______, KC_INS,  _______, _______, QK_MAKE, KC_BRID, KC_BRIU, _______, _______,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* ADJUST
