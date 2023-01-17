511c26d5ff51de8d2d55d46e71337b65f6defa09
Sam Boysel <sboysel@gmail.com>
2023-01-16 17:37:09 -0800

    [lily58] keymap tweaks

--- keyboards/lily58/keymaps/sboysel/keymap.c
+++ keyboards/lily58/keymaps/sboysel/keymap.c
@@ -19,17 +19,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| RAISE |    |BackSP |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LCtl | LGUI |LOWER | /Space  /       \Enter \  |RAISE | RGUI | RAlt |
+ *                   | LCtl | LGUI |LOWER | /Space  /       \Enter \  |RAISE |RShift| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,       KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______, KC_BSPC, KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                       KC_LCTL, KC_LGUI, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_RGUI, KC_RALT
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,     KC_BSLS,
+  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_RAISE), KC_BSPC, KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                       KC_LCTL, KC_LGUI, MO(_LOWER), KC_SPC,     KC_ENT,  MO(_RAISE), KC_RSFT, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  Del |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |  -   |  =   |  [   |  ]   |      |-------.    ,-------| Home | PgDn | PgUp | End  |      |  -   |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|       |    | Del   |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      | Mute | Vol- | Vol+ |      |  =   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \BackSP\  |      |      |      |
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   _  |   +  |   {  |   }  |      |-------.    ,-------| Left | Down |  Up  |Right |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * | Caps |      |      |      |      |      |-------|    |-------|      |      |Brght-|Brght+|      |      |
+ * |------+------+------+------+------+------|Insert |    | Del   |------+------+------+------+------+------|
+ * | Caps |      |      |      |      |      |-------|    |-------| Home | PgDn | PgUp | End  |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-  KC_CAPS, _______, _______, _______, _______, _______, KC_INS,  _______, _______, QK_MAKE, KC_BRID, KC_BRIU, _______, _______,
+  KC_CAPS, _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* ADJUST
