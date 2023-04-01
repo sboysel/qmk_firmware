ea1623c2f9d55f702bc87b597e647e475b06501b
Sam Boysel <sboysel@gmail.com>
2023-04-01 16:35:45 -0400

    adding fingerpuch

---
+++ keyboards/fingerpunch/personal/euclid36proto/rules.mk
@@ -0,0 +1,42 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+
+# DEFERRED_EXEC_ENABLE = yes
+# SRC +=  keyboards/fingerpunch/src/fp.c \
+#         keyboards/fingerpunch/src/fp_haptic.c \
+#         keyboards/fingerpunch/src/fp_audio.c \
+#         keyboards/fingerpunch/src/fp_keyhandler.c \
+#         keyboards/fingerpunch/src/fp_pointing.c \
+#         keyboards/fingerpunch/src/fp_rgb_common.c \
+#         keyboards/fingerpunch/src/fp_rgblight.c \
+#         keyboards/fingerpunch/src/fp_rgb_matrix.c
